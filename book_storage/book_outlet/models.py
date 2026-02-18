4987206b24dbdc086a0cb6b97810a5419890f2d7
Kostya Betenya <betenya.kostya@mail.ru>
2026-02-18 13:03:34 +0300

    feat: add work with relations

--- book_storage/book_outlet/models.py
+++ book_storage/book_outlet/models.py
@@ -3,15 +3,50 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, unique=True, db_index=True)
+    slug = models.SlugField(default='', null=False, blank=True, unique=True, db_index=True)
+    publish_countries = models.ManyToManyField(Country, null=True, related_name='books')
 
     def get_absolute_url(self):
         return reverse('book-page', args=[self.id])
